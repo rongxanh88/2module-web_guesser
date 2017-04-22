6834b7b378fe585f7ccc71048482b4a08eb372c1
rongxanh88 <rongxanh88@gmail.com>
2017-04-22 09:30:45 -0600

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+class WebGuesser
+  attr_reader :hidden_number
+
+  def initialize
+    @hidden_number = rand(100)
+  end
+
+end
+
+wb = WebGuesser.new
 
 get '/' do
-  "Hello, World!"
+  "The secret number is X, where the secret number is #{wb.hidden_number}."
 end
