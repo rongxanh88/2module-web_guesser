d1771a85ebeeda44b9aef99b5fcc7e3ca9651ef6
rongxanh88 <rongxanh88@gmail.com>
2017-04-23 10:47:10 -0600

    full functionality prior to refactor

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,10 +8,32 @@ class WebGuesser
     @hidden_number = rand(100)
   end
 
+  def check_guess(number)
+    if number.nil?
+      message = ""
+    elsif number == hidden_number
+      message = "Your guess is correct!"
+    elsif number < hidden_number
+      message = "Your guess is too low"
+    elsif number > hidden_number
+      message = "Your guess is too high"
+    end
+    message
+  end
+
 end
 
 wb = WebGuesser.new
 
 get '/' do
-  erb :index, :locals => {:hidden_number => wb.hidden_number}
+  message = ""
+  erb :index, :locals => {
+    :hidden_number => wb.hidden_number, :message => message
+  }
+
+  guessed_number = params["guess"]
+  message = wb.check_guess(guessed_number.to_i)
+  erb :index, :locals => {
+    :hidden_number => wb.hidden_number, :message => message
+  }
 end
