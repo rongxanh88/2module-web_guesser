6fc33f639b3b6a6903d54dfd6aac3ab0020562b6
rongxanh88 <rongxanh88@gmail.com>
2017-04-23 11:16:44 -0600

    ful functionality after refactor

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,22 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 class WebGuesser
-  attr_reader :hidden_number
+  attr_reader :hidden_number, :number_x
 
   def initialize
     @hidden_number = rand(100)
+    @number_x = "X"
   end
 
   def check_guess(number)
-    if number.nil?
+    if number == 0
       message = ""
     elsif number == hidden_number
-      message = "Your guess is correct!"
+      message = "You got it right!"
+      @number_x = hidden_number
+    elsif number <= (hidden_number - 5)
+      message = "Way too low!"
+    elsif number >= (hidden_number + 5)
+      message = "Way too high!"
     elsif number < hidden_number
-      message = "Your guess is too low"
+      message = "Too low!"
     elsif number > hidden_number
-      message = "Your guess is too high"
+      message = "Too high!"
     end
     message
   end
@@ -26,14 +33,10 @@ end
 wb = WebGuesser.new
 
 get '/' do
-  message = ""
-  erb :index, :locals => {
-    :hidden_number => wb.hidden_number, :message => message
-  }
-
   guessed_number = params["guess"]
   message = wb.check_guess(guessed_number.to_i)
+  
   erb :index, :locals => {
-    :hidden_number => wb.hidden_number, :message => message
+    :hidden_number => wb.number_x, :message => message
   }
 end
